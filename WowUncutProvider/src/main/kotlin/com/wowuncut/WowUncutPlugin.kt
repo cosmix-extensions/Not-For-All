262b804d50d9f42b473bd09eea3c6642989f20de
Ayon <senkuboy0@gmail.com>
2026-06-05 13:31:35 +0600

    Fix load override signature

--- WowUncutProvider/src/main/kotlin/com/wowuncut/WowUncutPlugin.kt
+++ WowUncutProvider/src/main/kotlin/com/wowuncut/WowUncutPlugin.kt
@@ -2,11 +2,10 @@ package com.wowuncut
 import com.cosmix.app.plugins.CsxPlugin
 import com.cosmix.app.plugins.CsxPluginAnnotation
 
-import android.content.Context
 
 @CsxPluginAnnotation
 class WowUncutPlugin : CsxPlugin() {
-    override fun load(context: Context) {
+    override fun load() {
         registerCsxApi(WowUncutProvider())
     }
 }
