1be96ebf39d824954dad0928aeee3e992185ebc8
Ayon <senkuboy0@gmail.com>
2026-06-05 13:51:22 +0600

    Fix WowUncutProvider config and add GPL-3.0 License

--- WowUncutProvider/build.gradle.kts
+++ WowUncutProvider/build.gradle.kts
@@ -1,11 +1,14 @@
+version = 1
+
 cosmix {
     setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/cosmix-extensions/Not-For-All")
-    version = 1
     description = "WowUncut - Hindi, Bengali, Tamil & More Web Series"
     authors = listOf("cosmix-extensions")
     language = "hi"
     tvTypes = listOf("TvSeries", "Movie", "Others")
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://wowuncut.com/&size=128"
+    status = 1
+    isCrossPlatform = true
 }
 
 android {
