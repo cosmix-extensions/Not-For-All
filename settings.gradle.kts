090d7f1519ad301d52147771d829411dc501372c
Ayon <senkuboy0@gmail.com>
2026-06-09 17:29:31 +0600

    Remove Mat6tubeProvider extension

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "NSFW Providers"
-include(":MlsbdProvider", ":ViralLinksProvider", ":WowProvider", ":WowUncutProvider", ":HamsterProvider", ":Mat6tubeProvider")
+include(":MlsbdProvider", ":ViralLinksProvider", ":WowProvider", ":WowUncutProvider", ":HamsterProvider")
